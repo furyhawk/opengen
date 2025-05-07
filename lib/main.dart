57d146c1e73abf2195c668af9700907b11e0355e
furyhawk <furyx@hotmail.com>
2025-05-07 21:57:14 +0800

    Add OpenAPI annotation for login API generation

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:openapi_generator_annotations/openapi_generator_annotations.dart';
+
 
 void main() {
   runApp(const MainApp());
 }
 
+@Openapi(
+  additionalProperties:
+      DioProperties(pubName: 'login_api', pubAuthor: 'furyhawk'),
+  inputSpec:
+      RemoteSpec(path: 'https://service.furyhawk.lol/api/v1/openapi.json'),
+  generatorName: Generator.dio,
+  runSourceGenOnOutput: true,
+  skipIfSpecIsUnchanged: false,
+  outputDirectory: 'api/login_api',
+)
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
